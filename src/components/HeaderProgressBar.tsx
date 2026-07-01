bbc025c116f85d594475f64269a94b7d0b097797
Eunjin Oh <ibsuny3214@naver.com>
2026-07-02 01:40:08 +0900

    feat: 헤더 진행바 컴포넌트 제작 #3

--- src/components/HeaderProgressBar.tsx
+++ src/components/HeaderProgressBar.tsx
@@ -6,7 +6,7 @@ export default function HeaderProgressBar({ step }: HeaderProgressBarProps) {
   const progressPercentage = 100 / 3;
 
   return (
-    <div className="w-full flex gap-0.75 items-center justify-between h-1">
+    <div className="w-full flex gap-0.75 items-center justify-between h-1 mt-3.75">
       {[1, 2, 3].map((currentStep) => (
         <div
           key={currentStep}

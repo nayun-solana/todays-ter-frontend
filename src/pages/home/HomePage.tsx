491ceb9cd7c8a13c0e7bf37928374312a74f6a48
Snagwoo <kimsu3047@gmail.com>
2026-07-04 01:59:06 +0900

    feat: 라우팅·앱 셸·페이지 스캐폴드 (#10)

---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <div className="p-5">홈 (준비 중)</div>;
+}
---
+++ src/pages/my/MyPage.tsx
@@ -0,0 +1,3 @@
+export default function MyPage() {
+  return <div className="p-5">마이 (준비 중)</div>;
+}

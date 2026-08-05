037eb3397841ad920722654d71bd86e10bbdee22
Eunjin Oh <ibsuny3214@naver.com>
2026-08-05 22:08:03 +0900

    feat: 사주 리포트 텍스트 박스 컴포넌트 리팩토링 #80

--- src/pages/onboarding/components/ContentBox.tsx
+++ src/pages/onboarding/components/ContentBox.tsx
@@ -1,14 +1,33 @@
+type Type = 'TOP' | 'COLOR' | 'TEXT' | 'GENETAL' | 'CATEGORY';
+
 type Props = {
   title?: string;
   children?: React.ReactNode;
+  type: Type;
+  ohangColor?: string;
+  categoryColor?: string;
 };
 
-export default function ContentBox({ title, children }: Props) {
+export default function ContentBox({ title, children, type, ohangColor, categoryColor }: Props) {
   return (
-    <div className="rounded-btn bg-white shadow-[0_2px_4px_0_rgba(0,0,0,0.10)] flex flex-col gap-4 p-5">
-      {/* Head 4 */}
-      {title && <p className="text-sm font-extrabold text-gray-6">{title}</p>}
-      {children}
+    <div
+      className={`rounded-btn shadow-[0_2px_4px_0_rgba(0,0,0,0.10)] flex flex-col ${type === 'COLOR' ? 'bg-primary gap-3 p-5' : 'bg-white gap-4'} ${type == 'TOP' ? 'p-5' : 'p-4.5'} ${type === 'GENETAL' ? `border border-${ohangColor}` : ''} ${type === 'CATEGORY' ? `border border-${categoryColor}` : ''}`}
+    >
+      {type === 'TEXT' && (
+        <>
+          {title && <p className="typo-head-4 text-gray-6">{title}</p>}
+          {children}
+        </>
+      )}
+      {type === 'COLOR' && (
+        <>
+          <p className="typo-caption text-primary-light">핵심 요약</p>
+          {children}
+        </>
+      )}
+      {type === 'GENETAL' && <>{children}</>}
+      {type === 'CATEGORY' && <>{children}</>}
+      {type === 'TOP' && <>{children}</>}
     </div>
   );
 }
